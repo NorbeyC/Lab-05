316df30c0c883528599a5aba1f8369ecd17d4fb2
unknown <norbey.cardona@labinfo.is.escuelaing.edu.co>
2022-03-11 12:54:45 -0500

    avance 2.0

--- web/src/main/java/edu/eci/cvds/servlet/Sample2Servlet.java
+++ web/src/main/java/edu/eci/cvds/servlet/Sample2Servlet.java
@@ -53,7 +53,41 @@ public class Sample2Servlet extends HttpServlet{
              responseWriter.write(message);
          }
  
-    }     
+    } 
     
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Writer responseWriter = resp.getWriter();
+        String message = "";
+        try{
+            Optional<String> optId = Optional.ofNullable(req.getParameter("id") );
+            Todo todo = Service.getTodo( Integer.parseInt(optId.get()) );
+            resp.setStatus(HttpServletResponse.SC_OK);
+
+            ArrayList<Todo> todoList = new ArrayList<>();
+            todoList.add(todo);
+            message =Service.todosToHTMLTable(todoList);
+        }
+        catch ( FileNotFoundException fileNotFoundException){
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            message = "no existe un item con el identificador dado.";
+        }
+        catch( NumberFormatException numberFormatException){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            message = "requerimiento inválido";
+        }
+        catch( MalformedURLException malformedURLException){
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            message = "error interno en el servidor";
+        }
+        catch(Exception exception){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            message = "requerimiento inválido";
+        }
+        finally {
+            responseWriter.write(message);
+        }
+    }
+     
 }
    
